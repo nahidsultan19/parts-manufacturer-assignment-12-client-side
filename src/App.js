6a23713540ca7f0b3d6808af177ba69a8bf381af
nahidsultan19 <nahidsultan38@gmail.com>
2022-05-21 23:05:57 +0600

    install dependency and added Header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Header from './Shared/Header';
 
 function App() {
   return (
     <div>
-      <h2>Project Setup</h2>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<p>About</p>} />
+        <Route path='/services' element={<p>Services</p>} />
+        <Route path='/login' element={<p>Login</p>} />
+      </Routes>
     </div>
   );
 }
